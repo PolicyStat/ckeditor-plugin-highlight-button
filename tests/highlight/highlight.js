56acf6dd787c33c3722fc60e7a091f2b56ec0230
Alex Tsai <caffodian@gmail.com>
2017-05-02 10:39:08 -0700

    test ideas

---
+++ tests/highlight/highlight.js
@@ -0,0 +1,13 @@
+/* bender-tags: editor,unit */
+/* bender-ckeditor-plugins: highlight,toolbar */
+
+bender.editor = { config: {} };
+
+var cmdName = "highlight";
+
+bender.test({
+  "test basic highlight": function() {},
+  "test no-op with collapsed selection": function() {},
+  "test multi element selection": function() {},
+  "test partial overlapping selection": function() {}
+});
