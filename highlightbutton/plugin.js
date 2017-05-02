5364db63ae4e18a04d3ea54cc5e942318fe9c01e
Alex Tsai <caffodian@gmail.com>
2017-05-02 10:19:58 -0700

    bring over initial plugin version

--- highlightbutton/plugin.js
+++ highlightbutton/plugin.js
@@ -1,9 +1,31 @@
-'use strict';
+"use strict";
+(function() {
+  CKEDITOR.plugins.add("highlightbutton", {
+    requires: "toolbar",
+    icons: "highlight",
+    highlightStyleDef: {
+      name: "Highlight",
+      element: "span",
+      attributes: { class: "highlight" }
+    },
+    init: function(editor) {
+      var highlightStyle = new CKEDITOR.style(this.highlightStyleDef);
+      var commandName = "highlight";
+      editor.addCommand(commandName, new CKEDITOR.styleCommand(highlightStyle));
 
-(function () {
-	CKEDITOR.plugins.add('highlightbutton', {
-		init: function (editor) {
+      editor.attachStyleStateChange(highlightStyle, function(state) {
+        !editor.readOnly && editor.getCommand(commandName).setState(state);
+      });
 
-		}
-	});
+      // Add toolbar button for this command.
+      editor.ui.addButton &&
+        editor.ui.addButton("highlightButton", {
+          label: "Highlight",
+          // toolbar: 'insert,10'
+          // You may want to set some icon here.
+          // icon: 'someIcon'
+          command: commandName
+        });
+    }
+  });
 })();
