584188ff467df7acb4d988f19358921e9f25d535
Alex Tsai <caffodian@gmail.com>
2017-05-11 09:04:29 -0700

    remove placeholder comments

--- highlightbutton/plugin.js
+++ highlightbutton/plugin.js
@@ -18,13 +18,9 @@
         !editor.readOnly && editor.getCommand(commandName).setState(state);
       });
 
-      // Add toolbar button for this command.
       editor.ui.addButton &&
         editor.ui.addButton("highlightButton", {
           label: "Highlight",
-          // toolbar: 'insert,10'
-          // You may want to set some icon here.
-          // icon: 'someIcon'
           command: commandName
         });
     }
